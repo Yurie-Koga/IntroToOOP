724e79b34014a95059766e07f2bf365024b572f9
Yurie-Koga <54040614+Yurie-Koga@users.noreply.github.com>
2020-10-05 16:35:12 -0700

    Add files via upload

---
+++ src/basics/Hello.java
@@ -0,0 +1,17 @@
+package basics;
+
+import org.w3c.dom.css.CSSStyleSheet;
+
+public class Hello {
+    // psvm + [tab]
+    public static void main(String[] args) {
+        System.out.println("basics.Hello");    // sout + [tab]
+        System.out.print("this is");
+        System.out.print(" a line without break\n");
+
+        // %s: string
+        // %02d: int(digit) with padding 2 zeros
+        // %f: float
+        System.out.printf("%s class starts at %d:%02d pm\n", "Java", 1, 0);
+    }
+}
