755b710c091d334a55ddd134358ec3afa89a8b09
Yurie <yurie.koga.a@gmail.com>
2020-10-08 14:39:01 -0700

    Added Car class

---
+++ src/oop/basic/Car.java
@@ -0,0 +1,61 @@
+package oop.basic;
+// way 1: no constructor in Car class + new Car() -> use JAVA's default constructor
+// way 2: add default constructor w/o empty value + new Car() -> use this default constructor
+// way 3: add constructor with values + new Car(value1, value2,..) -> use this constructor with values
+
+public class Car {
+    // ('this' refers here) : set of variables (states)
+    String color;
+    String brand;
+    int year;
+    int hoursePower;
+    int seats;
+
+    // (way1) if there is no constructors, JAVA gives a default constructor
+
+    // (way2) specify default constructor: initialize instance variables
+    // - if no parameters are passed when new Car(), these below values are set
+    Car() {
+        this.color = "Red";
+        this.brand = "Ferrari";
+        this.year = 2019;
+        this.hoursePower = 540;
+        this.seats = 2;
+    }
+
+    // (way3) constructor
+//    Car(String c, String b, int y, int hp, int s) {   // parameters should be the same same as instance variables
+//        color = c;
+//        brand = b;
+//        year = y;
+//        hoursePower = hp;
+//        seats = s;
+//    }
+    Car(String color, String brand, int year, int hoursePower, int seats) {
+        // 'this' refers to the instance that's being used = above variables ^^
+        this.color = color;
+        this.brand = brand;
+        this.year = year;
+        this.hoursePower = hoursePower;
+        this.seats = seats;
+    }
+
+    // set of methods (behaviors)
+    // (method1)
+    void drive() {
+        System.out.println("Driving.." + this.brand);
+    }
+    // 'method overloading'
+    // - create methods with the same name with different set of parameters
+    // (method2)
+    void drive(int speed) {
+        System.out.println("Driving " + this.brand + " at " + speed + " km/h");
+    }
+
+    void stop() {
+        System.out.println("Stopping..");
+    }
+
+
+}
+
---
+++ src/oop/basic/Driver.java
@@ -0,0 +1,32 @@
+package oop.basic;
+// Main Class / Client Class
+public class Driver {
+    public static void main(String[] args) {
+        // Car instance (object)
+        // new Car(); -> calls constructor (special method)
+        // - if no constructor is given in Car class, JAVA gives you 'default constructor'
+        // - default constructor: constructor without parameters
+
+        Car c1 = new Car(); // (way2) use default constructor, but override instance variables after creating object
+        c1.color = "Yellow";
+        c1.brand = "Lamborghini";
+        c1.year = 2020;
+        c1.hoursePower = 550;
+        c1.seats = 2;
+
+        System.out.print("c1 drive: ");
+        c1.drive();
+        c1.stop();
+
+        Car c2 = new Car(); // (way2) use default constructor w/o parameters = instance variables are initialized at constructor
+
+        System.out.println("c1 brand: " + c1.brand);
+        System.out.println("c2 brand: " + c2.brand);
+
+        Car c3 = new Car("Back", "Toyota", 2015, 250, 5);   // (way3) use constructor with values
+        System.out.println("c3 brand: " + c3.brand);
+        System.out.print("c3 drive: ");
+        c3.drive();            // (method1)
+        c3.drive(100);  // (method2)
+    }
+}
