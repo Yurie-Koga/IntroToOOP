0102830e19fe8c845d53b768be652b8b043d7ee7
Yurie <yurie.koga.a@gmail.com>
2020-10-13 22:18:46 -0700

    updated final variables name in ModelValidation.java

--- src/lab1/ModelValidation.java
+++ src/lab1/ModelValidation.java
@@ -1,28 +1,28 @@
 package lab1;
 
 public class ModelValidation {
-    public static final int nameMinLen = 3;
-    public static final int nameMaxLen = 20;
-    public static final int heightMinVal = 24;
-    public static final int heightMaxVal = 84;
-    public static final double wightMinVal = 80;
-    public static final double wightMaxVal = 280;
+    public static final int NAME_MIN_LEN = 3;
+    public static final int NAME_MAX_LEN = 20;
+    public static final int HEIGHT_MIN_VAL = 24;
+    public static final int HEIGHT_MAX_VAL = 84;
+    public static final double WIGHT_MIN_VAL = 80;
+    public static final double WIGHT_MAX_VAL = 280;
 
     public static void checkName(String name) {
-        if (name.length() < nameMinLen || name.length() > nameMaxLen) {
-            throw new IllegalArgumentException(String.format("Name length should be %d-%d", nameMinLen, nameMaxLen));
+        if (name.length() < NAME_MIN_LEN || name.length() > NAME_MAX_LEN) {
+            throw new IllegalArgumentException(String.format("Name length should be %d-%d", NAME_MIN_LEN, NAME_MAX_LEN));
         }
     }
 
     public static void checkHeight(int height) {
-        if (height < heightMinVal || height > heightMaxVal) {
-            throw new IllegalArgumentException(String.format("Height value should be %d-%d", heightMinVal, heightMinVal));
+        if (height < HEIGHT_MIN_VAL || height > HEIGHT_MAX_VAL) {
+            throw new IllegalArgumentException(String.format("Height value should be %d-%d", HEIGHT_MIN_VAL, HEIGHT_MIN_VAL));
         }
     }
 
     public static void checkWeight(double weight) {
-        if (weight < wightMinVal || weight > wightMaxVal) {
-            throw new IllegalArgumentException(String.format("weight value should be %f-%f", wightMinVal, wightMaxVal));
+        if (weight < WIGHT_MIN_VAL || weight > WIGHT_MAX_VAL) {
+            throw new IllegalArgumentException(String.format("weight value should be %f-%f", WIGHT_MIN_VAL, WIGHT_MAX_VAL));
         }
     }
 }
