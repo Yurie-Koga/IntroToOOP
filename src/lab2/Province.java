2399a682a2f2857148b95143439a36bafb726b3b
Yurie <yurie.koga.a@gmail.com>
2020-10-14 22:24:53 -0700

    added ProvinceValidation.java

--- src/lab2/Province.java
+++ src/lab2/Province.java
@@ -1,8 +1,28 @@
 package lab2;
 
+import static lab2.ProvinceValidation.*;
+
 public class Province {
-    private String name;        // e.g. “British Columbia”
+    //////////////////////////////////////////////////////////
+    private static final String DEFAULT_PROVINCE = "British Columbia";
+    private static final String DEFAULT_CAPITAL = "Victoria";
+    private static final long DEFAULT_POPULATION = 4648055;
+    //////////////////////////////////////////////////////////
+    private String province;    // e.g. “British Columbia”
     private String capital;     // e.g. “Victoria”
     private long population;    // e.g. 2264823
+
+    public Province(String province, String capital, long population) {
+        if (isValidPopulation(population) && isValidProvince(province) && (isValidCapitalCity(capital))) {
+            this.province = province;
+            this.capital = capital;
+            this.population = population;
+        } else {
+            this.province = DEFAULT_PROVINCE;
+            this.capital = DEFAULT_CAPITAL;
+            this.population = DEFAULT_POPULATION;
+        }
+    }
+
 
 }
---
+++ src/lab2/ProvinceValidation.java
@@ -0,0 +1,15 @@
+package lab2;
+
+public class ProvinceValidation {
+    public static boolean isValidProvince(String province) {
+        return false;
+    }
+
+    public static boolean isValidCapitalCity(String capital) {
+        return false;
+    }
+
+    public static boolean isValidPopulation(long population) {
+        return false;
+    }
+}
