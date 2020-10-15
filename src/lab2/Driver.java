6bfe0442ef2c75714a131f0614be34fd427b6a84
Yurie <yurie.koga.a@gmail.com>
2020-10-15 16:20:07 -0700

    updated lab2 Driver.java

--- src/lab2/Driver.java
+++ src/lab2/Driver.java
@@ -4,17 +4,26 @@ import static lab2.ProvinceValidation.*;
 
 public class Driver {
     public static void main(String[] args) {
-//        System.out.println(isValidProvince("British"));
-//        System.out.println(isValidCapitalCity("Victoria"));
+        Canada canada = new Canada();
+        canada.displayAllProvinces();
+        System.out.println(canada.getNumOfProvincesBetween(1, 15));
 
-//        System.out.println("----- edge/invalid -----");
-//        System.out.println(isValidPopulation(29999));
-//        System.out.println(isValidPopulation(30000));
-//        System.out.println(isValidPopulation(15000000));
-//        System.out.println(isValidPopulation(15000001));
+//        System.out.println("========== my test ==========");
+//        myTest();
+    }
 
-//        Province p = new Province();
-//        System.out.println(p.getDetails());
+    private static void myTest() {
+        System.out.println(isValidProvince("British"));
+        System.out.println(isValidCapitalCity("Victoria"));
+
+        System.out.println("----- edge/invalid -----");
+        System.out.println(isValidPopulation(29999));
+        System.out.println(isValidPopulation(30000));
+        System.out.println(isValidPopulation(15000000));
+        System.out.println(isValidPopulation(15000001));
+
+        Province p = new Province();
+        System.out.println(p.getDetails());
 
 
         Canada c = new Canada();
