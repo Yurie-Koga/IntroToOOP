5caf5b2a8ca2a4da881082b67e3bcf4f9aed6d52
Yurie <yurie.koga.a@gmail.com>
2020-10-14 22:09:49 -0700

    added lab2

---
+++ src/lab2/Province.java
@@ -0,0 +1,8 @@
+package lab2;
+
+public class Province {
+    private String name;        // e.g. “British Columbia”
+    private String capital;     // e.g. “Victoria”
+    private long population;    // e.g. 2264823
+
+}
